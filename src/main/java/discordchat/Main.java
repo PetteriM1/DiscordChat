00ff278292ad7dabfffa7a8b00d403b6252f984b
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-05-07 19:00:39 +0300

    .

--- src/main/java/discordchat/Main.java
+++ src/main/java/discordchat/Main.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
-import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.TextChannel;
 
 public class Main extends PluginBase {
@@ -14,7 +13,6 @@ public class Main extends PluginBase {
     public boolean debug;
 
     public static JDA jda;
-    public static Guild server;
     public static TextChannel channel;
     public static Config config;
 
@@ -28,8 +26,6 @@ public class Main extends PluginBase {
         try {
             if (debug) getServer().getLogger().info("Logging in with bot token " + config.getString("botToken", "\u00A7cnull"));
             jda = new JDABuilder(AccountType.BOT).setToken(config.getString("botToken")).buildBlocking();
-            if (debug) getServer().getLogger().info("Set server id to " + config.getString("serverId", "\u00A7cnull"));
-            server = jda.getGuildById(config.getString("serverId"));
             if (debug) getServer().getLogger().info("Set server channel id to " + config.getString("channelId", "\u00A7cnull"));
             channel = jda.getTextChannelById(config.getString("channelId"));
             if (debug) getServer().getLogger().info("Registering events for DiscordListener");
