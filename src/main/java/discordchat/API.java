a2a5510e02bbaa791849930a46619460195ffcf0
Sleepybear <thomas.ucsc@gmail.com>
2019-12-08 15:24:59 -0800

    API Refactor

--- src/main/java/discordchat/API.java
+++ src/main/java/discordchat/API.java
@@ -6,8 +6,16 @@ import net.dv8tion.jda.api.entities.TextChannel;
 public class API {
 
     public static void sendMessage(String message) {
+        API.sendMessage(message, false);
+    }
+
+    public static void sendToConsole(String message) {
+        API.sendMessage(message, true);
+    }
+
+    public static void sendMessage(String message, boolean toConsole) {
         if (Main.jda != null) {
-            TextChannel channel = Main.jda.getTextChannelById(Main.channelId);
+            TextChannel channel = Main.jda.getTextChannelById(toConsole ? Main.consoleChannelId : Main.channelId);
             if (channel != null) {
                 channel.sendMessage(message).queue();
             } else if (Main.debug) {
@@ -18,16 +26,4 @@ public class API {
         }
     }
 
-    public static void sendToConsole(String message) {
-        if (Main.jda != null) {
-            TextChannel channel = Main.jda.getTextChannelById(Main.consoleChannelId);
-            if (channel != null) {
-                channel.sendMessage(message).queue();
-            } else if (Main.debug) {
-                Server.getInstance().getLogger().error("TextChannel for console is null");
-            }
-        } else if (Main.debug) {
-            Server.getInstance().getLogger().error("JDA is null");
-        }
-    }
 }
