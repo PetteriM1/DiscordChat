3aa7cdf794c0a3511ad8e83babbbace0003df3ab
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-07-16 17:07:22 +0300

    Don't send quit messages for players who never spawned

--- src/main/java/discordchat/PlayerListener.java
+++ src/main/java/discordchat/PlayerListener.java
@@ -25,7 +25,9 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
-        if (Main.config.getBoolean("quitMessages")) API.sendMessage(Main.config.getString("info_player_left").replace("%player%", e.getPlayer().getName()));
+        if (Main.config.getBoolean("quitMessages") && e.getPlayer().spawned) {
+            API.sendMessage(Main.config.getString("info_player_left").replace("%player%", e.getPlayer().getName()));
+        }
     }
 
     @EventHandler(ignoreCancelled = true)
